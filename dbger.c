80f645d4175c57809e4249feefc6350c85bcdf8f
shadow3d <shadowthreeD@gmail.com>
2024-07-04 15:42:43 +0800

    modify LOG_BY_UART for redirect printf()

--- dbger.c
+++ dbger.c
@@ -1,77 +1,16 @@
-/* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    dbger.c
-  * @brief   This file provides code for realise the log priority.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
-
-/* Includes ------------------------------------------------------------------*/
+ * @file 	dbger.c
+ * @author	shadowthreed@gmail.com
+ * @date	20240714
+ */
 #include "dbger.h"
 
 #if LOG_ENABLE && LOG_BY_UART
-#define LOG_BY_UART_IT      0       // send by interrupt seems NOT better. If want use IT mode, need enable in cubeMx and regenerate the project.
-
-    uint8_t logBuf[LOG_BUF_LEN];
-    
-    // if last print NOT finish, this print will output nothing
-    void log_print(uint8_t level, char* fmt, ...)
-    {
-        if(level > LOG_LEVEL) {
-            return;
-        }
-        
-        int ret;
-        va_list ParamList;
-        
-        va_start(ParamList, fmt);
-        ret = vsprintf((char*)logBuf, fmt, ParamList);
-        va_end(ParamList);
-        
-        if(ret < 0) {   // vsprintf() error
-            return;
-        }
-        
-        #if LOG_BY_UART_IT
-        HAL_UART_Transmit_IT(LOG_UART, logBuf, strlen((const char*)logBuf));        // send in interrupt mode, maybe can change to DMA mode
-        #else
-        HAL_UART_Transmit(LOG_UART, logBuf, strlen((const char*)logBuf), 1000);   // send in block mode
-        #endif
-        
-        #if LOG_BY_UART_IT
-        while(LOG_UART->gState == HAL_UART_STATE_BUSY_TX);              // wait to send finish. Can NOT wait before last HAL_UART_Transmit_IT(), but why?
-        //while(HAL_UART_GetState(LOG_UART) == HAL_UART_STATE_BUSY_TX);   // same as last line
-        #endif
-    }
-#elif LOG_ENABLE && LOG_BY_RTT
-    
-    void log_print(uint8_t level, char* fmt, ...)
-    {
-        if(level > LOG_LEVEL) {
-            return;
-        }
-        
-        int ret;
-        va_list ParamList;
-
-        va_start(ParamList, fmt);
-        ret = SEGGER_RTT_vprintf(0, fmt, &ParamList);
-        va_end(ParamList);
-        
-        if(ret < 0) {
-            // TODO:
-        }
-    }
-    
+#include "usart.h"
+int stdout_putchar (int ch)
+{
+	const uint8_t cha = ch;
+	HAL_UART_Transmit(&huart1, &cha, 1, 10);
+	return ch;
+}
 #endif
--- dbger.h
+++ dbger.h
@@ -1,128 +1,101 @@
-/* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file    dbger.h
-  * @brief   This file provides code for outputing the LOG by uart or RTT.
-             The priority levels as below:
-                1.[AST]:Assert      (Highest)
+ * @file	dbger.h
+ * @brief	log moduleThis file provides code for outputing the LOG by uart or RTT.
+ *          The priority levels as below:
+ 				1.[AST]:Assert      (Highest)
                 2.[ERR]:Error
                 3.[WAR]:Warnning
                 4.[INF]:Info
                 5.[DBG]:Debug
                 6.[VBS]:Verbose     (Lowest)
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 shadowthreed@gmail.com
-  * All rights reserved.
-  *
-  ******************************************************************************
-  */
-/* USER CODE END Header */
+ *
+ * @note	1. if LOG_BY_RTT, you can get the LOG in J-Link RTT Viewer;
+ *			2. if LOG_BY_UART, you should enable the STDOUT in MDK-ARM -> RTE -> Compiler -> I/O -> STDOUT(user);
+ *			3. if only LOG_BY_RTT or LOG_BY_UART, you can output the LOG by LOG_xxx() micro;
+ *			4. if LOG_BY_RTT and LOG_BY_UART, you can output the LOG to RTT by LOG_xxx(), and output the LOG to UART by printf();
+ *			5. if LOG_ENABLE is 0, you should disable STDOUT in MDK-ARM -> RTE -> Compiler -> I/O -> STDOUT.
+ *
+ * @author	shadowthreed@gmail.com
+ * @date	20240714
+ */
 
-/**
-  ******* HOW TO USE *********
-  * 0. add the files in this folder to current project
-  * 1. #include "dbger.h"
-  # 2. change the config in dbger.h: LOG_ENABLE, LOG_BY_RTT, LOG_LEVEL, LOG_COLOR_ENABLE. LOG_BUF_LEN & LOG_UART if LOG_BY_UART is 1.
-  * 3. call LOG_INIT() at init;     // Just need call once
-  * 4. LOG_DBG("HelloWorld! number[%d]\r\n", 1);
-  */
-
-/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __DBGER_H__
 #define __DBGER_H__
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-#include <stdint.h>
-
-#define LOG_ENABLE          1
-#define LOG_BY_RTT          1
-#define LOG_BY_UART         (!LOG_BY_RTT)
-#define LOG_LEVEL           6       // the priority less than or queal to LOG_LEVEL will be output
-#define LOG_COLOR_ENABLE    0
+#define LOG_ENABLE 	1
+#define LOG_BY_RTT 	0
+#define LOG_BY_UART 1
+#define LOG_LEVEL 	4 		// the priority less than or queal to LOG_LEVEL will be output
+#define LOG_COLOR_ENABLE 1
 
 #if LOG_ENABLE
-  #include <string.h>
-  #define __FILENAME__        (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)      // strrchr(str, ch): return the last position of ch in str, or NULL
+	#include <string.h>
+	#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__) 	// strrchr(str, ch): return the last position of ch in str, or NULL
 #endif
 
-#if LOG_ENABLE && LOG_COLOR_ENABLE    // set LOG color
-  #define COLOR_RED       "\033[31m"
-  #define COLOR_PINK      "\033[35m"
-  #define COLOR_YELLOW    "\033[33m"
-  #define COLOR_GREEN     "\033[32m"
-  #define COLOR_GRAY      "\033[37m"
-  #define COLOR_DEFAULT   "\033[0m"       // off, default white
+#if LOG_ENABLE && LOG_COLOR_ENABLE // set LOG color
+	#define COLOR_RED 		"\033[31m"
+	#define COLOR_PINK 		"\033[35m"
+	#define COLOR_YELLOW 	"\033[33m"
+	#define COLOR_GREEN 	"\033[32m"
+	#define COLOR_GRAY 		"\033[37m"
+	#define COLOR_DEFAULT 	"\033[0m" 	// off, default white
 #else
-  #define COLOR_RED
-  #define COLOR_PINK
-  #define COLOR_YELLOW
-  #define COLOR_GREEN
-  #define COLOR_GRAY
-  #define COLOR_DEFAULT
+	#define COLOR_RED
+	#define COLOR_PINK
+	#define COLOR_YELLOW
+	#define COLOR_GREEN
+	#define COLOR_GRAY
+	#define COLOR_DEFAULT
 #endif
 
 #if LOG_ENABLE && LOG_BY_UART
-
-  #include <stdarg.h>
-  #include <stdio.h>
-  #include "usart.h"
-  #define LOG_BUF_LEN       (256)
-  #define LOG_UART          (&huart3)
-  
-  #define LOG_INIT()                MX_USART3_UART_Init();
-  #define LOG(level, fmt, ...)      log_print(level, fmt, ##__VA_ARGS__)
-  
-  #define LOG_AST(fmt, ...)         LOG(1, COLOR_RED     "[AST:%s:%d] " fmt, __FILENAME__, __LINE__, ##__VA_ARGS__)
-  #define LOG_ERR(fmt, ...)         LOG(2, COLOR_PINK    "[ERR:%s:%d] " fmt, __FILENAME__, __LINE__, ##__VA_ARGS__)
-  #define LOG_WAR(fmt, ...)         LOG(3, COLOR_YELLOW  "[WAR:%s:%d] " fmt, __FILENAME__, __LINE__, ##__VA_ARGS__)
-  #define LOG_INF(fmt, ...)         LOG(4, COLOR_DEFAULT "[INF] "       fmt, ##__VA_ARGS__)
-  #define LOG_DBG(fmt, ...)         LOG(5, COLOR_GREEN   "[DBG] "       fmt, ##__VA_ARGS__)
-  #define LOG_VBS(fmt, ...)         LOG(6, COLOR_GRAY                   fmt, ##__VA_ARGS__)
-  
-  extern uint8_t logBuf[LOG_BUF_LEN];
-  
-#elif LOG_ENABLE && LOG_BY_RTT
-  #include "SEGGER_RTT.h"
-  #define LOG_INIT()                SEGGER_RTT_Init()
-  #define LOG(level, fmt, ...)      do {                                \
-                                        if(level <= 3) {                \
-                                            SEGGER_RTT_SetTerminal(0);  \
-                                        } else if(level <= 4) {         \
-                                            SEGGER_RTT_SetTerminal(1);  \
-                                        } else {                        \
-                                            SEGGER_RTT_SetTerminal(2);  \
-                                        }                               \
-                                        log_print(level, fmt, ##__VA_ARGS__);   \
-                                    } while(0)
-  
-  #define LOG_AST(fmt, ...)         LOG(1, COLOR_RED     "[AST:%s:%d] " fmt, __FILENAME__, __LINE__, ##__VA_ARGS__)
-  #define LOG_ERR(fmt, ...)         LOG(2, COLOR_PINK    "[ERR:%s:%d] " fmt, __FILENAME__, __LINE__, ##__VA_ARGS__)
-  #define LOG_WAR(fmt, ...)         LOG(3, COLOR_YELLOW  "[WAR:%s:%d] " fmt, __FILENAME__, __LINE__, ##__VA_ARGS__)
-  #define LOG_INF(fmt, ...)         LOG(4, COLOR_DEFAULT        				fmt, ##__VA_ARGS__)
-  #define LOG_DBG(fmt, ...)         LOG(5, COLOR_GREEN   								fmt, ##__VA_ARGS__)
-  #define LOG_VBS(fmt, ...)         LOG(6, COLOR_GRAY                   fmt, ##__VA_ARGS__)
-  
-#elif !LOG_ENABLE
-
-  #define LOG_INIT()
-  #define LOG_AST(fmt, ...)
-  #define LOG_ERR(fmt, ...)
-  #define LOG_WAR(fmt, ...)
-  #define LOG_INF(fmt, ...)
-  #define LOG_DBG(fmt, ...)
-  #define LOG_VBS(fmt, ...)
-
+	#include <stdio.h>
+	#include "RTE_Components.h"
+	#ifndef RTE_Compiler_IO_STDOUT_User
+		#error	must enable STDOUT in MDK-ARM -> RTE -> Compiler -> I/O -> STDOUT(user).
+	#endif
 #endif
 
-extern void log_print(uint8_t level, char* fmt, ...);
+#if LOG_ENABLE
+#if LOG_BY_RTT
+// SEGGER_RTT_SetTerminal(2); 	SEGGER_RTT_SetTerminal(3);  keep for user, eg:
+// #define LOG_DAT(...)	do { if(LOG_LEVEL >= 1) { SEGGER_RTT_SetTerminal(2); SEGGER_RTT_printf(0, ##__VA_ARGS__); SEGGER_RTT_SetTerminal(1); }} while(0)
+	#include "SEGGER_RTT.h"
+	#define LOG_INIT()			SEGGER_RTT_Init()
+	#define LOG_AST(...)    do { if(LOG_LEVEL >= 1) { SEGGER_RTT_SetTerminal(0); SEGGER_RTT_printf(0, COLOR_RED 		"[AST:%s:%d] ", __FILENAME__, __LINE__); SEGGER_RTT_printf(0, ##__VA_ARGS__); SEGGER_RTT_printf(0, COLOR_DEFAULT ""); SEGGER_RTT_SetTerminal(1); }} while(0)
+	#define LOG_ERR(...)    do { if(LOG_LEVEL >= 2) { SEGGER_RTT_SetTerminal(0); SEGGER_RTT_printf(0, COLOR_PINK 		"[ERR:%s:%d] ", __FILENAME__, __LINE__); SEGGER_RTT_printf(0, ##__VA_ARGS__); SEGGER_RTT_printf(0, COLOR_DEFAULT ""); SEGGER_RTT_SetTerminal(1); }} while(0)
+	#define LOG_WAR(...)    do { if(LOG_LEVEL >= 3) { SEGGER_RTT_SetTerminal(0); SEGGER_RTT_printf(0, COLOR_YELLOW 	"[WAR:%s:%d] ", __FILENAME__, __LINE__); SEGGER_RTT_printf(0, ##__VA_ARGS__); SEGGER_RTT_printf(0, COLOR_DEFAULT ""); SEGGER_RTT_SetTerminal(1); }} while(0)
+	#define LOG_INF(...)    do { if(LOG_LEVEL >= 4) { SEGGER_RTT_printf(0, ##__VA_ARGS__); }} while(0)
+	#define LOG_DBG(...)    do { if(LOG_LEVEL >= 5) { SEGGER_RTT_printf(0, ##__VA_ARGS__); }} while(0)
+	#define LOG_VBS(...)    do { if(LOG_LEVEL >= 6) { SEGGER_RTT_printf(0, ##__VA_ARGS__); }} while(0)
+#elif LOG_BY_UART
+	#include <stdio.h>
+	#define LOG_INIT()
+	#define LOG_AST(...)    do { if(LOG_LEVEL >= 1) { printf(COLOR_RED 		"[AST:%s:%d] ", __FILENAME__, __LINE__); printf(__VA_ARGS__); printf(COLOR_DEFAULT ""); }} while(0)
+	#define LOG_ERR(...)    do { if(LOG_LEVEL >= 2) { printf(COLOR_PINK 	"[ERR:%s:%d] ", __FILENAME__, __LINE__); printf(__VA_ARGS__); printf(COLOR_DEFAULT ""); }} while(0)
+	#define LOG_WAR(...)    do { if(LOG_LEVEL >= 3) { printf(COLOR_YELLOW "[WAR:%s:%d] ", __FILENAME__, __LINE__); printf(__VA_ARGS__); printf(COLOR_DEFAULT ""); }} while(0)
+	#define LOG_INF(...)    do { if(LOG_LEVEL >= 4) { printf(__VA_ARGS__); }} while(0)
+	#define LOG_DBG(...)    do { if(LOG_LEVEL >= 5) { printf(__VA_ARGS__); }} while(0)
+	#define LOG_VBS(...)    do { if(LOG_LEVEL >= 6) { printf(__VA_ARGS__); }} while(0)
+#endif
+#else
+	#define LOG_INIT()
+	#define LOG_AST(...)
+	#define LOG_ERR(...)
+	#define LOG_WAR(...)
+	#define LOG_INF(...)
+	#define LOG_DBG(...)
+	#define LOG_VBS(...)
+#endif
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __DBGER_H__ */
+#endif // __DBGER_H__
