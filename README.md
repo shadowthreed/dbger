8bd365a31e6fb6a882a335ad7edb56a4e0b101fb
ShadowD <ShadowThreeD@gmail.com>
2024-07-11 17:32:32 +0800

    using same API for RTT and Uart LOG

--- README.md
+++ README.md
@@ -4,9 +4,10 @@
 3. 可以配置日志输出级别；
 
 ## 使用说明
-1. 通过`git submodule add https://github.com/ShadowThree/dbger.git ThirdUtils/dbger`添加本项目到工程指定目录；
-2. 配置`dbger.h`；
-3. 在`main.c`中调用：
+1. 如果是通过`Uart`输出日志，通过`CubeMX`生成工程代码时，在`Project Manager --> Advanced Settings`中，在日志串口相应的初始化函数`MX_USARTx_UART_Init`后面的`Do Not Generate Function Call`打勾；
+2. 通过`git submodule add https://github.com/ShadowThree/dbger.git ThirdUtils/dbger`添加本项目到工程指定目录；
+3. 配置`dbger.h`；
+4. 在`main.c`中调用：
 ```c
 #include "dbger.h"
 
--- dbger.h
+++ dbger.h
@@ -2,21 +2,22 @@
  * @file	dbger.h
  * @brief	log moduleThis file provides code for outputing the LOG by uart or RTT.
  *          The priority levels as below:
- 				1.[AST]:Assert      (Highest)
+				1.[AST]:Assert      (Highest)
                 2.[ERR]:Error
                 3.[WAR]:Warnning
                 4.[INF]:Info
                 5.[DBG]:Debug
                 6.[VBS]:Verbose     (Lowest)
  *
- * @note	1. if LOG_BY_RTT, you can get the LOG in J-Link RTT Viewer;
- *			2. if LOG_BY_UART, you should enable the STDOUT in MDK-ARM -> RTE -> Compiler -> I/O -> STDOUT(user);
- *			3. if only LOG_BY_RTT or LOG_BY_UART, you can output the LOG by LOG_xxx() micro;
- *			4. if LOG_BY_RTT and LOG_BY_UART, you can output the LOG to RTT by LOG_xxx(), and output the LOG to UART by printf();
- *			5. if LOG_ENABLE is 0, you should disable STDOUT in MDK-ARM -> RTE -> Compiler -> I/O -> STDOUT.
+ * @note for Using this mode:
+ *		1. MUST enable the STDOUT in MDK-ARM -> RTE -> Compiler -> I/O -> STDOUT(user);
+ *		2. if LOG_BY_RTT, you can get the LOG in J-Link RTT Viewer;
+ *		3. if LOG_BY_UART, you can get the LOG in any UART assistant, like Putty;
+ *		4. you can output the LOG by LOG_xxx() micro for different LOG level, or just by printf();
  *
  * @author	shadowthreed@gmail.com
  * @date	20240714
+ *			20240809	update: same API for UART and RTT
  */
 
 #ifndef __DBGER_H__
@@ -27,18 +28,19 @@ extern "C"
 {
 #endif
 
-#define LOG_ENABLE 	1
-#define LOG_BY_RTT 	0
-#define LOG_BY_UART 1
-#define LOG_LEVEL 	4 		// the priority less than or queal to LOG_LEVEL will be output
-#define LOG_COLOR_ENABLE 1
+#define LOG_ENABLE 			1
+#define LOG_BY_RTT 			1
+#define LOG_BY_UART			(!LOG_BY_RTT)
+#define LOG_LEVEL 			6 		// the priority less than or queal to LOG_LEVEL will be output
+#define LOG_COLOR_ENABLE 	0
+#define LOG_TEST			0
 
 #if LOG_ENABLE
 	#include <string.h>
 	#define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__) 	// strrchr(str, ch): return the last position of ch in str, or NULL
 #endif
 
-#if LOG_ENABLE && LOG_COLOR_ENABLE // set LOG color
+#if LOG_ENABLE && LOG_COLOR_ENABLE 	// set LOG color
 	#define COLOR_RED 		"\033[31m"
 	#define COLOR_PINK 		"\033[35m"
 	#define COLOR_YELLOW 	"\033[33m"
@@ -54,7 +56,7 @@ extern "C"
 	#define COLOR_DEFAULT
 #endif
 
-#if LOG_ENABLE && LOG_BY_UART
+#if LOG_ENABLE
 	#include <stdio.h>
 	#include "RTE_Components.h"
 	#ifndef RTE_Compiler_IO_STDOUT_User
@@ -64,22 +66,24 @@ extern "C"
 
 #if LOG_ENABLE
 #if LOG_BY_RTT
-// SEGGER_RTT_SetTerminal(2); 	SEGGER_RTT_SetTerminal(3);  keep for user, eg:
-// #define LOG_DAT(...)	do { if(LOG_LEVEL >= 1) { SEGGER_RTT_SetTerminal(2); SEGGER_RTT_printf(0, ##__VA_ARGS__); SEGGER_RTT_SetTerminal(1); }} while(0)
+	// SEGGER_RTT_SetTerminal(2); 	SEGGER_RTT_SetTerminal(3);  keep for user, eg: terminal2 default for printing protocol data
 	#include "SEGGER_RTT.h"
-	#define LOG_INIT()			SEGGER_RTT_Init()
-	#define LOG_AST(...)    do { if(LOG_LEVEL >= 1) { SEGGER_RTT_SetTerminal(0); SEGGER_RTT_printf(0, COLOR_RED 		"[AST:%s:%d] ", __FILENAME__, __LINE__); SEGGER_RTT_printf(0, ##__VA_ARGS__); SEGGER_RTT_printf(0, COLOR_DEFAULT ""); SEGGER_RTT_SetTerminal(1); }} while(0)
-	#define LOG_ERR(...)    do { if(LOG_LEVEL >= 2) { SEGGER_RTT_SetTerminal(0); SEGGER_RTT_printf(0, COLOR_PINK 		"[ERR:%s:%d] ", __FILENAME__, __LINE__); SEGGER_RTT_printf(0, ##__VA_ARGS__); SEGGER_RTT_printf(0, COLOR_DEFAULT ""); SEGGER_RTT_SetTerminal(1); }} while(0)
-	#define LOG_WAR(...)    do { if(LOG_LEVEL >= 3) { SEGGER_RTT_SetTerminal(0); SEGGER_RTT_printf(0, COLOR_YELLOW 	"[WAR:%s:%d] ", __FILENAME__, __LINE__); SEGGER_RTT_printf(0, ##__VA_ARGS__); SEGGER_RTT_printf(0, COLOR_DEFAULT ""); SEGGER_RTT_SetTerminal(1); }} while(0)
-	#define LOG_INF(...)    do { if(LOG_LEVEL >= 4) { SEGGER_RTT_printf(0, ##__VA_ARGS__); }} while(0)
-	#define LOG_DBG(...)    do { if(LOG_LEVEL >= 5) { SEGGER_RTT_printf(0, ##__VA_ARGS__); }} while(0)
-	#define LOG_VBS(...)    do { if(LOG_LEVEL >= 6) { SEGGER_RTT_printf(0, ##__VA_ARGS__); }} while(0)
+	#include <stdint.h>
+	#define LOG_INIT()		SEGGER_RTT_Init()
+	#define LOG_DAT(...)	do { if(LOG_LEVEL >= 1) { SEGGER_RTT_SetTerminal(2); printf(##__VA_ARGS__); SEGGER_RTT_SetTerminal(1); }} while(0)		// for print protocol data
+	#define LOG_AST(...)    do { if(LOG_LEVEL >= 1) { SEGGER_RTT_SetTerminal(0); printf(COLOR_RED 		"[AST:%s:%d] ", __FILENAME__, __LINE__); printf(__VA_ARGS__); printf(COLOR_DEFAULT ""); SEGGER_RTT_SetTerminal(1); }} while(0)
+	#define LOG_ERR(...)    do { if(LOG_LEVEL >= 2) { SEGGER_RTT_SetTerminal(0); printf(COLOR_PINK 		"[ERR:%s:%d] ", __FILENAME__, __LINE__); printf(__VA_ARGS__); printf(COLOR_DEFAULT ""); SEGGER_RTT_SetTerminal(1); }} while(0)
+	#define LOG_WAR(...)    do { if(LOG_LEVEL >= 3) { SEGGER_RTT_SetTerminal(0); printf(COLOR_YELLOW 	"[WAR:%s:%d] ", __FILENAME__, __LINE__); printf(__VA_ARGS__); printf(COLOR_DEFAULT ""); SEGGER_RTT_SetTerminal(1); }} while(0)
+	#define LOG_INF(...)    do { if(LOG_LEVEL >= 4) { printf(__VA_ARGS__); }} while(0)
+	#define LOG_DBG(...)    do { if(LOG_LEVEL >= 5) { printf(__VA_ARGS__); }} while(0)
+	#define LOG_VBS(...)    do { if(LOG_LEVEL >= 6) { printf(__VA_ARGS__); }} while(0)
+	
 #elif LOG_BY_UART
 	#include <stdio.h>
-	#define LOG_INIT()
+	#define LOG_INIT()		MX_USART1_UART_Init()
 	#define LOG_AST(...)    do { if(LOG_LEVEL >= 1) { printf(COLOR_RED 		"[AST:%s:%d] ", __FILENAME__, __LINE__); printf(__VA_ARGS__); printf(COLOR_DEFAULT ""); }} while(0)
 	#define LOG_ERR(...)    do { if(LOG_LEVEL >= 2) { printf(COLOR_PINK 	"[ERR:%s:%d] ", __FILENAME__, __LINE__); printf(__VA_ARGS__); printf(COLOR_DEFAULT ""); }} while(0)
-	#define LOG_WAR(...)    do { if(LOG_LEVEL >= 3) { printf(COLOR_YELLOW "[WAR:%s:%d] ", __FILENAME__, __LINE__); printf(__VA_ARGS__); printf(COLOR_DEFAULT ""); }} while(0)
+	#define LOG_WAR(...)    do { if(LOG_LEVEL >= 3) { printf(COLOR_YELLOW 	"[WAR:%s:%d] ", __FILENAME__, __LINE__); printf(__VA_ARGS__); printf(COLOR_DEFAULT ""); }} while(0)
 	#define LOG_INF(...)    do { if(LOG_LEVEL >= 4) { printf(__VA_ARGS__); }} while(0)
 	#define LOG_DBG(...)    do { if(LOG_LEVEL >= 5) { printf(__VA_ARGS__); }} while(0)
 	#define LOG_VBS(...)    do { if(LOG_LEVEL >= 6) { printf(__VA_ARGS__); }} while(0)
@@ -96,6 +100,10 @@ extern "C"
 
 #ifdef __cplusplus
 }
+#endif
+
+#if LOG_TEST
+	void log_test(void);
 #endif
 
 #endif // __DBGER_H__
