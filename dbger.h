9f820e2e562206d671262f3c667dccd49430fdc9
shadow3d <shadowthreeD@gmail.com>
2024-08-16 17:29:44 +0800

    modify the default LOG_LEVEL to 5

--- dbger.h
+++ dbger.h
@@ -31,7 +31,7 @@ extern "C"
 #define LOG_ENABLE 			1
 #define LOG_BY_RTT 			1
 #define LOG_BY_UART			(!LOG_BY_RTT)
-#define LOG_LEVEL 			6 		// the priority less than or queal to LOG_LEVEL will be output
+#define LOG_LEVEL 			5 		// the priority less than or queal to LOG_LEVEL will be output
 #define LOG_COLOR_ENABLE 	0
 #define LOG_TEST			0
 
