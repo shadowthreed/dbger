c89ebfcd6f1a6b73cd05841e32b39f467a197f9a
shadow3d <shadowthreed@gmail.com>
2025-04-17 17:15:09 +0800

    add support for linux

--- dbger.c
+++ dbger.c
@@ -13,28 +13,45 @@
 #if LOG_BY_UART
 
 #include "usart.h"
+#if LOG_PLATFORM == 0		// MDK_ARM
 int stdout_putchar (int ch)
 {
-	const uint8_t cha = ch;
+	uint8_t cha = ch;
 	HAL_UART_Transmit(&huart1, &cha, 1, 10);
 	return ch;
 }
+#elif LOG_PLATFORM == 1		// Linux
+int __io_putchar(int ch, FILE *f) {
+	(void)f;
+	uint8_t cha = ch;
+	HAL_UART_Transmit(&huart1, &cha, 1, 10);
+	return ch;
+}
+#endif
 
 #elif LOG_BY_RTT
 
+#if LOG_PLATFORM == 0		// MDK_ARM
 int stdout_putchar (int ch)
 {
-	SEGGER_RTT_printf(0, "%c", ch);
+	SEGGER_RTT_PutChar(0, ch);
 	return ch;
 }
+#elif LOG_PLATFORM == 1		// Linux
+int __io_putchar(int ch, FILE *f) {
+	(void)f;
+	SEGGER_RTT_PutChar(0, ch);
+	return ch;
+}
+#endif
 
 #endif
 
-#if LOG_TEST
+#if LOG_TEST_EN
 void log_test(void)
 {
 	float valf = 3.14;
-	char str[] = "LOT TEST";
+	char str[] = "LOG TEST";
 	
 	LOG_INIT();
 	for(uint8_t i = 0; i < 2; i++) {
