4d670a73cdb63781fe1eea7b349aa96d48f432e3
shadow3d <shadowthreed@gmail.com>
2025-11-26 09:54:02 +0800

    support RTT CMD

--- dbger.c
+++ dbger.c
@@ -43,7 +43,32 @@ int __io_putchar(int ch, FILE *f) {
 	SEGGER_RTT_PutChar(0, ch);
 	return ch;
 }
-#endif
+#endif  // LOG_PLATFORM
+
+#if RTT_CMD_ENABLE
+char RTT_cmd_buf[RTT_CMD_BUF_LEN];
+size_t get_RTT_cmd(void)
+{
+    static size_t index = 0;
+    size_t cmd_len = 0;
+    int ch = SEGGER_RTT_GetKey();
+    if(ch != -1) {
+        if(ch == '\n' || ch == '\r') {
+            if(index > 0) {
+                RTT_cmd_buf[index] = '\0';
+                //LOG_DBG("%s\n", RTT_cmd_buf);
+                cmd_len = index;
+                index = 0;
+            }
+        } else {
+            if(index < (sizeof(RTT_cmd_buf) - 1)) {
+                RTT_cmd_buf[index++] = ch;
+            }
+        }
+    }
+    return cmd_len;
+}
+#endif  // RTT_CMD_ENABLE
 
 #endif
 
@@ -62,6 +87,18 @@ void log_test(void)
 		LOG_DBG("DBG LOG: int[%d], float[%f], str[%s]\n", i, valf, str);
 		LOG_VBS("VBS LOG: int[%d], float[%f], str[%s]\n", i, valf, str);
 		printf("\n");
+
+        size_t cmd_len = get_RTT_cmd();
+        if(cmd_len) {
+            LOG_DBG("cmd[%s] len[%d]\n", RTT_cmd_buf, cmd_len);
+            if(strncmp(RTT_cmd_buf, "test1", 5) == 0) {
+                LOG_DBG("process test1 cmd\n");
+            } else if(strncmp(RTT_cmd_buf, "test2", 5) == 0) {
+                LOG_DBG("process test2 cmd\n");
+            } else {
+                LOG_DBG("cmd[%s] NOT exist\n", RTT_cmd_buf);
+            }
+        }
 	}
 }
 #endif
